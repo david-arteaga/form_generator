3a420afe9cae54977660a80f3591ffdeb6e021dc
David Arteaga <david@Davids-MacBook-Pro.local>
2017-06-18 20:24:55 -0500

    remove reference in EntityFormFieldMapper

--- src/main/java/com/form_generator/mapping/thymeleaf/mapper/EntityFormFieldMapper.java
+++ src/main/java/com/form_generator/mapping/thymeleaf/mapper/EntityFormFieldMapper.java
@@ -41,28 +41,4 @@ public class EntityFormFieldMapper implements FormFieldMapper<EntityFormFieldTyp
         return formGroup;
     }
 
-    private final static String template =
-            "<div class=\"form-group\">\n" +
-            "    <select name=\"%s\" class=\"form-control\" >\n" +
-            "        <option selected disabled>%s </option>\n" +
-            "        <option " +
-                        "th:each=\"%s: ${%s}\" " +
-                        "th:value=\"${%s.%s}\" " +
-                        "th:text=\"${%s.%s}\">option label" +
-                        "</option>\n" +
-            "    </select>\n" +
-            "</div>";
-
-    public String renderField(FormField formField) {
-        Entity entity = formField.getFormFieldType().getEntity();
-        String idFieldName = entity.getIdFieldName();
-        return String.format(template,
-                formField.getFieldSingularName(),
-                formField.getFieldSingularLabel(),
-
-                formField.getFieldSingularName(), formField.getFieldPluralName(),
-                formField.getFieldSingularName(), idFieldName,
-                formField.getFieldSingularName(), entity.getNameFieldName());
-    }
-
 }
